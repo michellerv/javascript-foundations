9f05539a7bbf240d1f7e63b12697f1660a13eee9
Michelle Villagran <mrvillagran16@gmail.com>
2023-05-16 17:32:14 -0600

    Feature: Add functions greeetRider and eat

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -2,17 +2,35 @@ function createDragon(name, rider, temperment) {
   return {
     name:name,
     rider:rider,
-    temperment: temperment
+    temperment: temperment,
+    timesEaten: 0,
+    hungry: true
   }
 }
 
-function greetRider() {
-return 
+function greetRider(dragon) {
+return `Hi, ${dragon.rider}!`
+}
+
+function eat(dragon) {
+  dragon.timesEaten++
+  if (dragon.timesEaten >= 3) {
+    dragon.hungry = false 
+  }
+  return dragon
+}
+
+function findFireBreathers(allDragons) {
+  //Create an array of dragons. Start with empty array.
+  //Check each dragon. If aggressive, put on list. 
+  //If not aggressive, don't put on list.
+  //Return new array of aggressive dragons.
+return allDragons
 }
 
 module.exports = {
   createDragon, 
   greetRider, 
-  // eat, 
-  // findFireBreathers
+  eat, 
+  findFireBreathers
 }
