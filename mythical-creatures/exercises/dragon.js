0bc8fcb9a7de6e6d33f708b6f6adb719937128eb
Michelle Villagran <mrvillagran16@gmail.com>
2023-05-18 08:41:21 -0600

    Add comments fireBreathers function

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -20,15 +20,22 @@ function eat(dragon) {
   return dragon
 }
 
-//Created a function called findFireBreathers that takes in a paramter
-//This parameter 
+//Created a function called findFireBreathers that takes in a parameter
+//This parameter is speaking to the argument of allDragons which contains the dragon array of objects.
 function findFireBreathers(dragonArray) {
+//created a new variable called newFire array that would hold the new array of agressive dragons. 
   var newFireArray = []
   for (var i = 0; i < dragonArray.length; i++) {
+    //This for loop will go through the aray of dragon objects.
       if (dragonArray[i].temperment.includes('aggressive')) {
+      //If an object in the dragon array has a temperment of aggrassive
+      //should be included in the new array
          newFireArray.push(dragonArray[i])
+      //The new Fire array should have any dragon object with an aggressive temperment pushed 
+      //into it. 
       }
     }  return newFireArray
+    //return the new array of aggessive dragons
   }
   
 
